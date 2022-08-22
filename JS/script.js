0be730b432ed80d434266e87202d630348fa4a30
Onyiana <adaurecananaba@gmail.com>
2022-08-22 20:45:54 +0100

    updated

--- JS/script.js
+++ JS/script.js
@@ -2,19 +2,27 @@
 let date = document.getElementById("date")
 date.innerHTML = new Date().getFullYear()
 
+// This was used to add click event on the toggler and also a blur
 const menuOpen = document.querySelector('.ri-menu-add-line')
 const menuClose = document.querySelector('.ri-close-circle-fill')
 const navMenu = document.querySelector('.navlinks')
+const wraper = document.querySelector('.wraper')
+const html = document.querySelector('html')
+
 menuOpen.addEventListener('click', function(){
     navMenu.style.left = '50%'
     menuOpen.style.display = 'none'
     menuClose.style.display = 'block'
+    wraper.classList.add('blur')
+    html.classList.add('hideScroll')
 })
 
 menuClose.addEventListener('click', function(){
     navMenu.style.left = '-50%'
     menuOpen.style.display = 'block'
     menuClose.style.display = 'none'
+    wraper.classList.remove('blur')
+    html.classList.remove('hideScroll')
 })
 
 const navLists = document.querySelectorAll('li')
@@ -23,10 +31,14 @@ navLists.forEach((element) => {
         navMenu.style.left = '-50%'
         menuClose.style.display = 'none'
         menuOpen.style.display = 'block'
+        html.classList.remove('hideScroll')
+        wraper.classList.remove('blur')
     })
 })
 
 
+
+// libray for sliding
 $(".owl-carousel").owlCarousel({
     items: 4,
     loop: true,
